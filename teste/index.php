48a72618813bee4ac4045d06f3e250252c97a92d
Adauto Nobre <contato@showsite.com.br>
2018-11-26 14:35:16 -0200

    Test Fix

--- teste/index.php
+++ teste/index.php
@@ -4,9 +4,9 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "mail.showsite.com.br", "contato@showsite.com.br", "Showsite2509#", "ssl", "465",
-    "contato@showsite.com.br", "Show Site");
-$novoEmail->sendMail("Email de teste 2", "<p>Mensagem do email de <b>teste 2</b></p>", "financeiro@showsite.com.br",
-    "Financeiro", "adautonobrejr@gmail.com", "Junior");
+$ mail = new Email (2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)",
+    "from@email.com", "From Name");
+
+$ email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
